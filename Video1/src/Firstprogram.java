480733130ba9ee3993eac466d4690fcbbe5d2fa0
gandhi <gandhi@gandhi-PC>
2016-07-01 08:26:51 +0530

    hhhhhh

---
+++ Video1/src/Firstprogram.java
@@ -0,0 +1,55 @@
+
+public class Firstprogram {
+
+	public static void main(String[] args) {
+		System.out.println("this is first program");
+		
+		//data types
+		
+		int i=100; //integer // 32 bit 
+		System.out.println(i);
+		 i=1000;
+		System.out.println(i);
+		
+		long z=1987; //integer // 64 bit
+		System.out.println(z);
+		
+		double d=100.98 ;// integers and decimals
+		double dd=99;
+		System.out.println(d);
+		System.out.println(dd);
+		
+		char c='e';
+		boolean b=true;//true and false values 
+		
+		System.out.println(99);
+		System.out.println(2>1);
+		System.out.println(2>11);
+		// System.out.println(9==9);
+		
+		System.out.println("*****************************");
+		
+		if (true){
+			System.out.println("pass");
+		}
+		
+		int k=100;
+		int j=200;
+		if (j>k)
+		{
+			System.out.println("Value of j is "+j);//+ is contanated operator 
+		}
+		}
+	
+	//String is not a datatype it is a class
+	String str ="Hello how are you";
+	
+		
+				
+		
+		
+		
+
+	}
+
+
